b563757ceb9afc0e05443e8eed8d40fa43df09c1
Yasin Shamrat <yshamrat@gmail.com>
2019-12-05 15:40:10 +0600

    data added in the model class

--- Quizzler-iOS13/QuestionBank.swift
+++ Quizzler-iOS13/QuestionBank.swift
@@ -12,7 +12,17 @@ class QuestionBank {
     var list = [Questions]()
     
     init() {
-        list.append(Questions(question: "2 + 2 = 2", answer: false))
+        list.append(Questions(question: "Approximately one quarter of human bones are in the feet.", answer: true))
         list.append(Questions(question: "A slug's blood is green.", answer: true))
+        list.append(Questions(question:"The total surface area of two human lungs is approximately 70 square metres.", answer:true))
+        list.append(Questions(question:"In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: true))
+        list.append(Questions(question:"In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: false))
+        list.append(Questions(question: "It is illegal to pee in the Ocean in Portugal.", answer: true))
+        list.append(Questions(question: "You can lead a cow down stairs but not up stairs.", answer: false))
+        list.append(Questions(question: "Google was originally called 'Backrub'.", answer: true))
+        list.append(Questions(question: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: true))
+        list.append(Questions(question: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: true))
+        list.append(Questions(question: "No piece of square dry paper can be folded in half more than 7 times.", answer: false))
+        list.append(Questions(question: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: true))
     }
 }
